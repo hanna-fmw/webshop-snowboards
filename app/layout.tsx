5706aa4c900a89dd374a01f0a987df06c9b53df6
Hanna Hosk <hanna.hosk@gmail.com>
2024-01-17 16:52:28 +0100

    update navbar.module.css, globals.css and layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local'
 
 import type { Metadata } from 'next'
-import { Inter, Roboto_Mono } from 'next/font/google'
+import { Inter, Roboto_Mono,Sometype_Mono, Red_Hat_Mono, Fira_Code  } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/molecules/navbar/Navbar'
 
