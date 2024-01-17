f45e3c8fc0f8bf98bffeed289c1939091cd2f925
Hanna Hosk <hanna.hosk@gmail.com>
2024-01-17 10:05:52 +0100

    Initial commit

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './HeroSection.module.css'
+import { unstable_getImgProps as getImgProps } from 'next/image'
+
+const { heroContainer, heroImage, heroText } = styles
+
+const HeroSection = () => {
+	//unstable_getImgProps to be able to use srcSet to change image based on screen size in latest Next.js version
+	const {
+		props: { srcSet: desktop },
+	} = getImgProps({ src: '/StartPageHero_1.png', alt: 'Hero Image Desktop', width: '1440', height: '400' })
+	const {
+		props: { srcSet: mobile },
+	} = getImgProps({ src: '/HeroMobile.png', alt: 'Hero Image Mobile', width: '600', height: '400' })
+
+	return (
+		<div className={heroContainer}>
+			<picture>
+				<source media='(max-width:567px)' srcSet={mobile} />
+				<source media='(min-width: 568px)' srcSet={desktop} />
+				<img src={desktop} alt='Hero image' className={heroImage} />
+			</picture>
+		</div>
+	)
+}
+
+export default HeroSection
---
+++ app/components/ProductContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './ProductContainer.module.css'
+
+const ProductContainer = () => {
+	return <div>This is the Product Grid</div>
+}
+
+export default ProductContainer
