fec1d6ed782dbfce2add1b0ea9bf1e2ac03ffef2
Hanna Hosk <hanna.hosk@gmail.com>
2024-01-18 15:32:00 +0100

    navbar mobile

--- app/components/molecules/heroSection/HeroSection.tsx
+++ app/components/molecules/heroSection/HeroSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './heroSection.module.css'
-import { unstable_getImgProps as getImgProps } from 'next/image'
+import Image, { unstable_getImgProps as getImgProps } from 'next/image'
 
 const { heroContainer, heroImage, heroText } = styles
 
@@ -8,18 +8,20 @@ const HeroSection = () => {
 	//unstable_getImgProps to be able to use srcSet to change image based on screen size in latest Next.js version
 	const {
 		props: { srcSet: desktop },
-	} = getImgProps({ src: '/hero/StartPageHero_1.png', alt: 'Hero Image Desktop', width: '1440', height: '400' })
+	} = getImgProps({ src: '/hero/hero_desktop.png', alt: 'Hero Image Desktop', width: '1440', height: '937' })
 	const {
 		props: { srcSet: mobile },
-	} = getImgProps({ src: '/hero/Hero-Mobile.png', alt: 'Hero Image Mobile', width: '600', height: '400' })
+	} = getImgProps({ src: '/hero/hero_mobile.png', alt: 'Hero Image Mobile', width: '390', height: '580' })
 
 	return (
 		<div className={heroContainer}>
-			<picture>
-				<source media='(max-width:567px)' srcSet={mobile} />
-				<source media='(min-width: 568px)' srcSet={desktop} />
+			{/* <picture>
+				<source media='(max-width:768px)' srcSet={mobile} />
+				<source media='(min-width: 769px)' srcSet={desktop} />
 				<img src={desktop} alt='Hero image' className={heroImage} />
-			</picture>
+			</picture> */}
+
+			<Image src='/hero/hero_desktop.png' width={1440} height={937} className={heroImage} alt='Hero image' />
 		</div>
 	)
 }
--- app/components/molecules/navbar/Navbar.tsx
+++ app/components/molecules/navbar/Navbar.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import styles from './navbar.module.css'
+import Image from 'next/image'
 
 const Navbar = () => {
-	return <nav className={styles.nav}>Navbar</nav>
+	return (
+		<nav className={styles.navContainer}>
+			<Image src='/logo/TUR_Logotype.png' width={157} height={26} alt='TUR Logotype' />
+			<div className={styles.navbarItems}>
+				<Image src='/icons/hamburger_menu_mobile.png' width={22} height={20} alt='Cart icon' />
+				<div className={styles.cartItems}>
+					<Image src='/icons/cart.png' width={15} height={17} alt='Cart icon' />
+					<small className={styles.cartItemCount}>[ 7 ]</small>
+				</div>
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
