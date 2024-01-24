e1414bd85c31f267a55185b9ecc1e4266c372495
Hanna Hosk <hanna.hosk@gmail.com>
2024-01-24 10:44:54 +0100

    Navbar story

---
+++ app/components/molecules/navbar/Navbar.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import styles from './navbar.module.css'
+import Image from 'next/image'
+
+import Navbar from './Navbar'
+
+const meta = {
+	title: '/molecules/Navbar',
+	component: Navbar,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+} satisfies Meta<typeof Navbar>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+	render: (args) => <Navbar />,
+}
--- app/components/molecules/navbar/Navbar.tsx
+++ app/components/molecules/navbar/Navbar.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 import styles from './navbar.module.css'
 import Image from 'next/image'
 import NavLink from '../../atoms/navLink/NavLink'
+import hamburgerMenu from '../../../../public/icons/hamburger_menu_mobile.png'
+import logotype from '../../../../public/logo/logo.png'
+import cart from '../../../../public/icons/cart.png'
 
-const Navbar = () => {
+type NavbarProps = {
+	children?: React.ReactNode
+}
+
+const Navbar = ({ children }: NavbarProps) => {
 	return (
 		<nav className={styles.navContainer}>
-			<Image src='/logo/logo.png' width={157} height={26} alt='TUR Logotype' />
+			{/* <Image src='/logo/logo.png' width={157} height={26} alt='TUR Logotype' /> */}
+			<Image src={logotype} width={157} height={26} alt='TUR Logotype' className={styles.logo} />
 			<div className={styles.navItems}>
 				<div className={styles.navContainer}>
 					<NavLink url='/'>[a]. HOME</NavLink>
@@ -14,13 +22,17 @@ const Navbar = () => {
 					<NavLink url='/'>[c]. ABOUT</NavLink>
 					<NavLink url='/'>[d]. SUPPORT</NavLink>
 				</div>
+				{children}
 
 				<div className={styles.cartItems}>
-					<Image src='/icons/cart.png' width={15} height={17} alt='Cart icon' />
+					{/* <Image src='/icons/cart.png' width={15} height={17} alt='Cart icon' /> */}
+					<Image src={cart} width={15} height={17} alt='Cart icon' />
 					<small className={styles.cartItemCount}>[ 7 ]</small>
 				</div>
 			</div>
-			<Image src='/icons/hamburger_menu_mobile.png' width={22} height={20} alt='Cart icon' className={styles.hamburgerMenu} />
+
+			{/* <Image src='/icons/hamburger_menu_mobile.png' width={22} height={20} alt='Cart icon' className={styles.hamburgerMenu} /> */}
+			<Image src={hamburgerMenu} width={22} height={20} className={styles.hamburgerMenu} alt='Hamburger Menu icon' />
 		</nav>
 	)
 }
