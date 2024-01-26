484a8be85eae7638350d30f872f425bb1a8d6fcb
Hanna Hosk <hanna.hosk@gmail.com>
2024-01-26 13:57:52 +0100

    Currency Dropdown and Navbar adjustments

--- app/components/atoms/currencyDropdown/CurrencyDropdown.tsx
+++ app/components/atoms/currencyDropdown/CurrencyDropdown.tsx
@@ -1,16 +1,50 @@
 'use client'
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
+import { RiArrowDownSLine } from 'react-icons/ri'
 import styles from './currencyDropdown.module.css'
 
-const CurrencyDropdown = () => {
+function CurrencyDropdown() {
+	const [value, setValue] = useState('SEK')
+	const [isOpen, setIsOpen] = useState(false)
+
+	const handleMouseEnter = () => {
+		setIsOpen(true)
+	}
+
+	const handleMouseLeave = () => {
+		setIsOpen(false)
+	}
+
+	const handleItemClick = (newValue: string) => {
+		setValue(newValue)
+		setIsOpen(false)
+	}
+
 	return (
-		<>
-			<ul style={{ listStyle: 'none', padding: '0' }}>
-				<li>SEK</li>
-				<li>EUR</li>
-			</ul>
-		</>
+		<div className={styles.dropdownContainer} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+			<button className={styles.button}>
+				<div style={{ marginRight: '5px' }}>{value}</div>
+				<div>
+					<RiArrowDownSLine size={18} style={{ color: '#212121', transform: 'translateY(10%)' }} />
+				</div>
+			</button>
+			{isOpen && (
+				<div className={styles.dropdown}>
+					<div className={styles.menuItems}>
+						{value === 'SEK' && (
+							<div className={`${styles.menuItem} ${styles.activeItem}`} onClick={() => handleItemClick('EUR')}>
+								EUR
+							</div>
+						)}
+						{value === 'EUR' && (
+							<div className={`${styles.menuItem} ${styles.activeItem}`} onClick={() => handleItemClick('SEK')}>
+								SEK
+							</div>
+						)}
+					</div>
+				</div>
+			)}
+		</div>
 	)
 }
 
