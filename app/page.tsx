173db79a644085e7a9afbf0912ff0b92216fa404
Hanna Hosk <hanna.hosk@gmail.com>
2024-02-09 15:17:14 +0100

    Added animation to products card on Home page

--- app/page.tsx
+++ app/page.tsx
@@ -9,21 +9,9 @@ import TextBlock from './components/atoms/textBlock/TextBlock'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
 
-const parentVariants = {
-	initial: { opacity: 1 },
-	animate: {
-		opacity: 1,
-
-		transition: {
-			staggerChildren: 0.2,
-			duration: 0.5,
-		},
-	},
-}
-
-const childrenVariants = {
-	initial: { opacity: 0 },
-	animate: { opacity: 1 },
+const motionProps = {
+	initial: { opacity: 0, y: 100 },
+	animate: (i: number) => ({ opacity: 1, y: 0, transition: { delay: 0.2 * i } }),
 }
 
 export default function Home() {
@@ -35,76 +23,29 @@ export default function Home() {
 					<div>TUR SNOWBOARD PRODUCTS:</div>
 					<Link href='/shop'>&gt; SHOW ALL</Link>
 				</div>
-				<motion.div className={styles.productCardContainer} variants={parentVariants} initial="initial" animate="animate">
-					{/* <ProductGrid> */}
-					{/* {products.map((product, i) => {
+				<ProductGrid>
+					{products.map((product, i) => {
+						const featured = product.featured === true
 						return (
-							<ProductCard key={i}>
-								<Figure image={`/products/${product.image}`} />
-								<TextBlock
-									model={product.model}
-									designer={product.designer}
-									length={product.length}
-									detail={product.detail}
-									profile={product.profile}
-									price={product.price}
-									currency='SEK'
-								/>
-							</ProductCard>
+							<motion.div key={i} variants={motionProps} initial='initial' whileInView='animate' viewport={{ once: true }} custom={i}>
+								{featured ? (
+									<ProductCard>
+										<Figure image={`/products/${product.image}`} />
+										<TextBlock
+											model={product.model}
+											designer={product.designer}
+											length={product.length}
+											detail={product.detail}
+											profile={product.profile}
+											price={product.price}
+											currency='SEK'
+										/>
+									</ProductCard>
+								) : null}
+							</motion.div>
 						)
-					})} */}
-
-					<ProductCard>
-						<Figure image='/products/bubo154/BUBO154.png' />
-
-						<TextBlock
-							model={products[0].model}
-							designer={products[0].designer}
-							length={products[0].length}
-							detail={products[0].detail}
-							profile={products[0].profile}
-							price={products[0].price}
-						/>
-					</ProductCard>
-
-					<ProductCard>
-						<Figure image='/products/bubo150/BUBO150.png' />
-						<TextBlock
-							model={products[1].model}
-							designer={products[1].designer}
-							length={products[1].length}
-							detail={products[1].detail}
-							profile={products[1].profile}
-							price={products[1].price}
-						/>
-					</ProductCard>
-
-					<ProductCard>
-						<Figure image='/products/labb/LABB.png' />
-						<TextBlock
-							model={products[2].model}
-							designer={products[2].designer}
-							length={products[2].length}
-							detail={products[2].detail}
-							profile={products[2].profile}
-							price={products[2].price}
-						/>
-					</ProductCard>
-
-					<ProductCard>
-						<Figure image='/products/falc/FALC.png' />
-						<TextBlock
-							model={products[3].model}
-							designer={products[3].designer}
-							length={products[3].length}
-							detail={products[3].detail}
-							profile={products[3].profile}
-							price={products[3].price}
-						/>
-					</ProductCard>
-				</motion.div>
-
-				{/* </ProductGrid> */}
+					})}
+				</ProductGrid>
 			</section>
 		</main>
 	)
